59964bfa5948ee2d1e467e36947b814a02e9ace2
ephrem <ephremmamo555@gmail.com>
2024-04-13 17:37:53 +0300

    degree centrality function finished

--- graph_centrailities.py
+++ graph_centrailities.py
@@ -18,5 +18,28 @@ class GraphCentrality:
                 adj_matrix[i, j] = weight
             
         return adj_matrix
+    def degree_centrality(self,graph):
+        degree_centralities = {}
+        number_of_nodes = len(graph.nodes)
+        
+        
+        # Iterate through a hash map that contains node and its neighbours
+        for node in graph.nodes:
+            degree_centralities[node] = len(graph.nodes[node].neighbours)/number_of_nodes
+            
+        #Initialized list that holds nodes with degree centrality
+        top_degree_centralities = []
+        top = max(degree_centralities.values())
+
+        for centrality in degree_centralities:
+            if degree_centralities[centrality] == top:
+                top_degree_centralities.append(centrality)
+        
+
+        print("The Vertices with Highest degree centrality are", *top_degree_centralities)
+        
+
+        # Final degree Centrality
+        return degree_centralities
     
     
