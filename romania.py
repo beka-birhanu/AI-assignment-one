09ee10bb11269e01b78eb4c77857380183d3e862
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-04-13 23:24:38 +0300

    add centranlity calculation on romania

--- romania.py
+++ romania.py
@@ -1,4 +1,5 @@
 from graph import Graph
+from graph_centrailities import *
 from search_algorithms import *
 from utils import compute_heuristic
 
@@ -17,15 +18,46 @@ with open('edges.txt', 'r') as file:
         city1, city2, weight = line.strip().split()
         romania.add_edge(city1, city2, float(weight))
 
+
 if __name__ == "__main__":
-    g = romania
-    for node1 in g.nodes.values():
-        for node2 in g.nodes.values():
-            print(breadth_first_search(g, node1.name, node2.name))
-            print(iterative_deepening_search(g, node1.name, node2.name, 30))
-            print(uniform_cost_search(g, node1.name, node2.name))
-            print(bidirectional_search(
-                g, node1.name, node2.name, compute_heuristic))
-            print(greedy_search(g, node1.name, node2.name, compute_heuristic))
-            print(a_star_search(g, node1.name, node2.name, compute_heuristic))
-            print(depth_first_search(g, node1.name, node2.name), '\n')
+    for node1 in romania.nodes.values():
+        for node2 in romania.nodes.values():
+            if node1.name == node2.name:
+                continue
+
+            print(
+                f"Breadth First Search: {breadth_first_search(romania, node1.name, node2.name)}\n")
+            print(
+                f"Iterative Deepening Search: {iterative_deepening_search(romania, node1.name, node2.name, 30)}\n")
+            print(
+                f"Uniform Cost Search: {uniform_cost_search(romania, node1.name, node2.name)}\n")
+            print(
+                f"Bidirectional Search: {bidirectional_search(romania, node1.name, node2.name, compute_heuristic)}\n")
+            print(
+                f"Greedy Search: {greedy_search(romania, node1.name, node2.name, compute_heuristic)}\n")
+            print(
+                f"A* Search: {a_star_search(romania, node1.name, node2.name, compute_heuristic)}\n")
+            print(
+                f"Depth First Search: {depth_first_search(romania, node1.name, node2.name)}\n\n")
+
+    centrality, top_degree_nodes = degree_centrality(romania)
+    print(f"Degree Centrality: {centrality}, Top Nodes: {top_degree_nodes}\n")
+
+    centrality, top_closeness_nodes = closeness_centrality(romania)
+    print(
+        f"Closeness Centrality: {centrality}, Top Nodes: {top_closeness_nodes}\n")
+
+    centrality, top_eigenvector_nodes = eigenvector_centrality(romania)
+    print(
+        f"Eigenvector Centrality: {centrality}, Top Nodes: {top_eigenvector_nodes}\n")
+
+    centrality, top_pagerank_nodes = pagerank_centrality(romania)
+    print(
+        f"Pagerank Centrality: {centrality}, Top Nodes: {top_pagerank_nodes}\n")
+
+    centrality, top_katz_nodes = katz_centrality(romania)
+    print(f"Katz Centrality: {centrality}, Top Nodes: {top_katz_nodes}\n")
+
+    centrality, top_betweenness_nodes = betweenness_centrality(romania)
+    print(
+        f"Betweenness Centrality: {centrality}, Top Nodes: {top_betweenness_nodes}\n")
